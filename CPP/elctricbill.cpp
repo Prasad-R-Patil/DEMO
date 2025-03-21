2baf9454e3752aeb3c48714622105ea843cf3a95
Prasad Patil <115949363+Prasad-R-Patil@users.noreply.github.com>
2025-03-21 17:47:38 +0530

    Add files via upload

--- CPP/elctricbill.cpp
+++ CPP/elctricbill.cpp
@@ -6,7 +6,7 @@ int main()
 
     float unit;
     float total=0.0f;	
-	cout<<" Enter unit :- "<<endl;
+	cout<<" Enter unit :- ";
 	cin>>unit;
 	
 	if (unit<=50)
@@ -23,7 +23,24 @@ int main()
 		total= (50*8.5)+( 200*13.00)+((unit-250)*18.25);
 	}
 	
-	cout<<" Total bill ="<<total<<endl;
+	
+	
+	
+	if (total<850)
+	{
+		total=850;
+		cout<<" Final Amount bill is =>> "<<total<<endl;
+	}
+	
+	else if(total>=1500)
+	{
+		int intrest = 0;
+		intrest=(total*15/100);
+		cout<<" Total bill %15 for extra =>> "<<intrest <<"  Total Bill is =>>"<<total+intrest<<endl;
+		cout<<" Final Amount bill is =>> "<<total+intrest<<endl;
+	}
+	
+	
 	
 	return 0;
 }
--- CPP/hotel.cpp
+++ CPP/hotel.cpp
@@ -2,66 +2,206 @@
 using namespace std;
 
 int main()
-{   
-
-	int ch =0;
-	int total =0;
-	int qut = 0;
-
+{
+	int ch=0;
+	int ch2=0;
+	int qut=0;
+	int qut2=0;
+	int total1=0;
+	int total2=0;
+	int total3=0;
+	int total4=0;
 	
-	do {
-			cout<<"1.vad-pav = 20"<<endl;
-			cout<<"2.samosa = 15"<<endl;
-			cout<<"3.tea = 10"<<endl;
-			cout<<"4.coffe = 12"<<endl;
-			cout<<"5.exit"<<endl;
+	do
+	{
+		cout<<"1.Snaks....||"<<endl;
+		cout<<"2.cold drinks"<<endl;
+		cout<<"3.Hot drinks|"<<endl;
+		cout<<"4.Ice-cream||"<<endl;
+		cout<<"5.Exit.....||"<<endl;
 		
-		cout<<"Enter Your Choice.... :- ";
+		cout<<"Enter Your Choice||"<<endl;
 		cin>>ch;
 		
 		switch(ch)
 		{
 			case 1:
 			{
-				cout<<"Add quantity...:- ";
-				cin>>qut;
+				do{
+					cout<<"1.Vada Pav.=20||"<<endl;
+					cout<<"2.samosa...=15||"<<endl;
+					cout<<"3.Exit........||"<<endl;
+					
+					cout<<"Enter Your Choice"<<endl;
+					cin>>ch2;
+					
+					switch(ch2)
+					{
+						case 1:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total1=total1+qut2*20;
+							break;
+						}
+						case 2:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total1=total1+qut2*15;
+							break;
+						}
+						default :
+						{
+							cout<<"Enter Valid Choice:-"<<endl;
+						}
+					}
+					
+					
+				}while(ch2!=3);
 				
-				total = total+qut*20;
+				
 				break;
+						
 			}
+			
 			case 2:
 			{
-				cout<<"Add quantity...:- ";
-				cin>>qut;
+				do{
+					cout<<"1.coca cola..=20||"<<endl;
+					cout<<"2.Papsi......=15||"<<endl;
+					cout<<"3.Exit..........||"<<endl;
+					
+					cout<<"Enter Your Choice"<<endl;
+					cin>>ch2;
+					
+					switch(ch2)
+					{
+						case 1:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total2=total2+qut2*20;
+							
+							break;
+						}
+						case 2:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total2=total2+qut2*15;
+							
+							break;
+						}
+						default :
+						{
+							cout<<"Enter Valid Choice:-"<<endl;
+						}
+					}
+					
+					
+				}while(ch2!=3);
 				
-				total = total+qut*15;
 				break;
+						
 			}
+			
 			case 3:
 			{
-				cout<<"Add quantity...:- ";
-				cin>>qut;
+				do{
+					cout<<"1.Tea......=20||"<<endl;
+					cout<<"2.Coffie...=15||"<<endl;
+					cout<<"3.Exit........||"<<endl;
+					
+					cout<<"Enter Your Choice"<<endl;
+					cin>>ch2;
+					
+					switch(ch2)
+					{
+						case 1:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total3=total3+qut2*20;
+							
+							break;
+						}
+						case 2:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total3=total3+qut2*15;
+							
+							break;
+						}
+						default :
+						{
+							cout<<"Enter Valid Choice:-"<<endl;
+						}
+					}
+					
+					
+				}while(ch2!=3);
 				
-				total = total+qut*10;
 				break;
+						
 			}
+			
 			case 4:
 			{
-				cout<<"Add quantity...:- ";
-				cin>>qut;
+				do{
+					cout<<"1.Venela.......=20||"<<endl;
+					cout<<"2.Butter scoh..=15||"<<endl;
+					cout<<"3.Exit............||"<<endl;
+					
+					cout<<"Enter Your Choice"<<endl;
+					cin>>ch2;
+					
+					switch(ch2)
+					{
+						case 1:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total4=total4+qut2*20;
+							
+							break;
+						}
+						case 2:
+						{
+							cout<<"Enter Quantity.:-"<<endl;
+							cin>>qut2;
+							total4=total4+qut2*15;
+							
+							break;
+						}
+						default :
+						{
+							cout<<"Enter Valid Choice:-"<<endl;
+						}
+					}
+					
+					
+				}while(ch2!=3);
 				
-				total = total+qut*12;
+				
 				break;
-			}
-			default :
-			{
-				cout<<"Enter Currect Choice..."<<endl;
+						
 			}
 		}
 		
 		
 		
 	}while(ch!=5);
+		
+	cout<<"1.Snaks.........||="<<total1<<endl;
+	cout<<"2.Cold Drinks...||="<<total2<<endl;
+	cout<<"3.Hot Drinks....||="<<total3<<endl;
+	cout<<"4.Ice-cream.....||="<<total4<<endl;
 	
-	cout<<"total bill ="<<total;
+	
+	cout<<"Total Bill......||="<<total1+total2+total3+total4;
+	
+	return 0;
+	
 }
