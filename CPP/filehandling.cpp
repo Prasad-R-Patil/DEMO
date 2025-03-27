70dc1d7f894763bc8952aa6074f5d1e17ca8ee8b
Prasad Patil <115949363+Prasad-R-Patil@users.noreply.github.com>
2025-03-27 17:55:32 +0530

    Add files via upload

---
+++ CPP/filehandling.cpp
@@ -0,0 +1,18 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+int main()
+{
+char ch;
+ifstream f1("king.txt");
+ f1.seekg(2,ios::beg);
+while(f1)
+{
+f1.get(ch);
+cout<<ch;
+/* if(ch=='E')
+ f1.seekg(1,ios::cur);
+ f1.get(ch);
+cout<<ch;
+*/}
+}
--- CPP/polyemp.cpp
+++ CPP/polyemp.cpp
@@ -47,8 +47,7 @@ class emp
 
 class manager:public emp
 {
-	
-	protected:
+
 	
 	float bonus;
 
@@ -81,12 +80,103 @@ class manager:public emp
 		
 };
 
+class clark:public emp
+{
+	
+	
+	int overtime;
+
+		public:
+		
+		clark()
+		{
+			overtime= 1;
+		}
+		
+		clark(int a)
+		{
+			overtime=a;
+		}
+		clark(int n,string nm,float bs,int o):emp(n,nm,bs)
+		{
+			overtime= o;
+		}
+		
+		float cal_sal()
+		{
+			bsal=bsal+overtime*500;
+			return bsal;
+		}
+		
+		~clark()
+		{
+			
+		}
+		
+};
+
+class programmer:public emp
+{
+	
+	
+	int insentive;
+
+		public:
+		
+		programmer()
+		{
+			insentive=5;
+		}
+		
+		programmer(int a)
+		{
+			insentive=a;
+		}
+		programmer(int n,string nm,float bs,int i):emp(n,nm,bs)
+		{
+			insentive=i;
+		}
+		
+		float cal_sal()
+		{
+			if(insentive<25)
+			{
+			  bsal=bsal+((bsal*5)/100);
+			  return bsal;
+			}
+			else if((insentive>=25) && (insentive<50))
+			{
+				bsal=bsal+((bsal*10)/100);
+			  return bsal;
+			}
+			else if((insentive>=50) && (insentive<75))
+			{
+				bsal=bsal+((bsal*15)/100);
+			  return bsal;
+			}
+			else if((insentive>=75) && (insentive<=100))
+			{
+				bsal=bsal+((bsal*25)/100);
+			  return bsal;
+			}
+			
+			
+		}
+		
+		~programmer()
+		{
+			
+		}
+		
+};
+
+
 int main()
 {
 	
 	//manager m(10,"sagar",5000.0,500.0);
 	
-	emp *ptr= new manager(10,"sagar",5000.0,500.0);
+	emp *ptr= new programmer(10,"sagar",100000.0,60);
 	
 	cout<<"bonus of emp = "<<ptr->cal_sal(); 
 	return 0;
